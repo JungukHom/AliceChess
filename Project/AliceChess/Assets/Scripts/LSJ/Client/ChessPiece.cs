48d8c721b51ae4ece765e646d45643b20651aeb1
JungukHom <developer0223@gmail.com>
2018-05-17 18:39:52 +0900

    2018-05-07-18:39(KOR)

--- Project/AliceChess/Assets/Scripts/LSJ/Client/ChessPiece.cs
+++ Project/AliceChess/Assets/Scripts/LSJ/Client/ChessPiece.cs
@@ -1,17 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using UnityEngine;
+using System.Collections;
 
-using static UnityEngine.Debug;
-
-namespace MainScene.LSJ
+namespace LSJ
 {
-    public class ChessPiece // MainScene.LSJ.ChessPiece
+    public class ChessPiece : MonoBehaviour
     {
-        public ChessPiece() { }
+        public float walkSpeed = 0.1f;
+        public float stopDistance = 0.1f;
 
-        public IEnumerator Move()
+        public Vector3 desPos = new Vector3(0, 0, 0.2f);
+
+        new private Transform transform;
+
+        private readonly string isWalk = "isWalk";
+        private readonly string isAttack = "isAttack";
+        private readonly string isDie = "isDie";
+
+        Animator anim;
+
+        private void Awake()
         {
-            Log($"Started Coroutine : LSJ.ChessPiece.Move()");
-            yield return null;
+            transform = GetComponent<Transform>();
         }
+
+        public IEnumerator StartMove(bool hasEnemy)
+        {
+            Debug.Log($"StartMove() / hasEnemy : {hasEnemy}");
+
+            anim.SetBool("isWalk", true);
+            anim.speed = 1.5f;
+
+            Vector3 dir = desPos - transform.position;
+
+            if (hasEnemy)
+            {
+                while(Vector3.Distance(transform.position, desPos / 2) > stopDistance)
+                {
+                    transform.position += walkSpeed * dir.normalized * Time.deltaTime;
+
+                    yield return new WaitForEndOfFrame();
+                }
+
+                anim.SetBool("isAttack", true);
+                yield return new WaitForEndOfFrame();
+
+                anim.SetBool("isAttack", false);
+
+                if(transform.position == desPos)
+                {
+                    anim.SetBool("isWalk", false);
+                }
+            }
+            else
+            {
+                while (Vector3.Distance(transform.position, desPos) > stopDistance)
+                {
+                    transform.position += walkSpeed * dir.normalized * Time.deltaTime;
+
+                    yield return new WaitForEndOfFrame();
+                }
+
+                transform.position = desPos;
+                anim.SetBool("isWalk", false);
+            }
+        }
+
     }
 }
